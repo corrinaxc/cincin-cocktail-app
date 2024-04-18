ae30378be3009c51286f086c35b71bc1bc8ab435
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-18 15:49:21 +0200

    install Adapter

---
+++ db/models/favourites.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const favouriteSchema = new Schema({
+    idDrink: {type: String},
+    strDrink: {type:String, required:true},
+    strInstructions: {type:String, required:true},
+    strIngredient1: {type:String, required:true},
+    strIngredient2: {type:String},
+    strIngredient3: {type:String},
+    strIngredient4: {type:String},
+    strIngredient5: {type:String},
+    strIngredient6: {type:String},
+    strIngredient7: {type:String},
+    strIngredient8: {type:String},
+    strIngredient9: {type:String},
+    strIngredient10: {type:String},
+    strIngredient11: {type:String},
+    strIngredient12: {type:String},
+    strIngredient13: {type:String},
+    strIngredient14: {type:String},
+    strIngredient15: {type:String},
+    strMeasure1: {type:String, required: true},
+    strMeasure2: {type:String},
+    strMeasure3: {type:String},
+    strMeasure4: {type:String},
+    strMeasure5: {type:String},
+    strMeasure6: {type:String},
+    strMeasure7: {type:String},
+    strMeasure8: {type:String},
+    strMeasure9: {type:String},
+    strMeasure10: {type:String},
+    strMeasure11: {type:String},
+    strMeasure12: {type:String},
+    strMeasure13: {type:String},
+    strMeasure14: {type:String},
+    strMeasure15: {type:String},
+    strDrinkThumb: {type:String},
+    isFavourite: {type:Boolean},
+});
+
+const Favourite = mongoose.models.Favourite || mongoose.model("Favourite", favouriteScheme);
+
+export default Favourite;
