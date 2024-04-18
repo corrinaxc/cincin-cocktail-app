73fca4a7c995c7e513911a77914668cd6fb73b36
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-18 13:36:06 +0200

    footer formatting

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -4,13 +4,15 @@ import Link from 'next/link';
 export default function Nav() {
     return(
         <footer>
-        <nav className="bg-white mt-3 w-screen text-center rounded-md display: flex justify-center gap-7">
+        <nav className="bg-white mt-3 w-screen text-center rounded-md display: flex justify-center gap-7 h-16">
             <Link href={'/'}>Home</Link>
-            <Link href={'/favourites'}>❤️</Link>
+            <Link className="w-14" href={'/favourites'}>❤️</Link>
             <Link href={'/login'}>
-                <img className="w-6" src='/resources/profile.png' />
+                <img className='w-6' src='/resources/profile.png' />
             </Link>
         </nav>
         </footer>
     )
 }
+
+// className="w-14"
