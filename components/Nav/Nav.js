bf50a4333d15bbe7642ce33ecaaa9ad034499667
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-18 12:17:31 +0200

    login icon for nav bar

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -4,10 +4,12 @@ import Link from 'next/link';
 export default function Nav() {
     return(
         <footer>
-        <nav className="bg-white mt-3 w-screen text-center rounded-md">
-            <p>Nav Links to Go Here</p>
+        <nav className="bg-white mt-3 w-screen text-center rounded-md display: flex justify-center gap-7">
             <Link href={'/'}>Home</Link>
             <Link href={'/favourites'}>❤️</Link>
+            <Link href={'/login'}>
+                <img className="w-6" src='/resources/profile.png' />
+            </Link>
         </nav>
         </footer>
     )
