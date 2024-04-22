675cc95d2e608db7d460cdac31ae26ca8e08a58f
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-22 13:29:37 +0200

    fixed favourite button for cocktail card

--- components/CocktailCard/CocktailCard.js
+++ components/CocktailCard/CocktailCard.js
@@ -2,6 +2,10 @@ import Link from "next/link"
 import { useRouter } from 'next/router';
 import FavouriteButton from "../FavouriteButton/FavouriteButton";
 import {useSession, signIn } from 'next-auth/react'
+import { useEffect, useState } from "react";
+import useSWR from 'swr';
+
+const fetcher = async (url) => await fetch(url).then((res) => res.json());
 
 export default function CocktailCard( {
     name,
@@ -11,9 +15,21 @@ export default function CocktailCard( {
     cocktailsInfo,
     idDrink
 }) {
-
+    const { data: session, status } = useSession();
+    const [favourites, setFavourites] = useState([]);
+    const { data, error, mutate } = useSWR(status === 'authenticated' ? `/api/favourites?userId=${session.user.id}` : null, fetcher);
     const router = useRouter();
-    // const {data: session, status} = useSession();
+  
+    useEffect(() => {
+      if (data) {
+        setFavourites(data);
+      }
+    }, [data]);
+  
+    if (status === 'loading') return <div>Loading...</div>;
+    if (error) return <div>Error fetching data</div>;
+    if (!favourites) return <div>No favourites found</div>;
+
 
     const handleBackButtonClick = () => {
         router.back();
@@ -26,7 +42,12 @@ export default function CocktailCard( {
         <div className="mb-8 rounded-md display: flex flex-col">
         <button onClick={handleBackButtonClick} className="ml-2">Back</button>
         {/* </Link> */}
-        <FavouriteButton name={name} image={image} cocktailsInfo={cocktailsInfo} idDrink={idDrink}/>
+        <FavouriteButton name={name}
+        image={image}
+        cocktailsInfo={cocktailsInfo}
+        idDrink={idDrink}
+        mutate={mutate}
+        favourites={favourites}/>
         <br></br>
         <img src={image}/>
         </div>
