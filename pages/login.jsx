8bedff67a4d1c85177f8310fd928247265aa8592
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-25 11:22:43 +0100

    fundamental styling

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-import {useSession, signIn, signOut} from 'next-auth/react'
+import { useSession, signIn, signOut } from 'next-auth/react'
+import { useRouter } from 'next/router';
 
 const login = () => {
-    const {data: session} = useSession();
+    const { data: session } = useSession();
+    const router = useRouter();
+
     console.log(session);
 
     if (session) {
+        router.push('/account');
+        return null; 
+    } else {
         return (
-            <div>
-            <p>Welcome, {session.user.name}</p>
-            <img src={session.user.image} alt="user-google-profile"/>
-            <button onClick = {() => signOut()}>Sign Out</button>
+            <div className="logInDiv">
+            <div className="glassHalfFullDiv">
+            <img className="w-10 text-center " src="/resources/halfempty.jpeg" alt="glasshalfempty-icon"/>
+            <h1>   Glass Half Empty!   </h1>
+            <img className="w-10" src="/resources/halfempty.jpeg" alt="glasshalfempty-icon"/>
             </div>
-        )
+            <div className="signInDiv">
+                <p><button className="signInButton" onClick={() => signIn()} >Sign In</button> to fill it up!</p>
+            </div>
+            </div>
+        );
     }
-else {
-    return (
-        <div>
-        <h1>Log In</h1>
-        <button onClick={() => signIn()} >Sign In</button>
-        </div>
-    )
-}
 }
 
-export default login
+export default login;
