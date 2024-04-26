f0becc0cd07e970dd725635f089d51ae3ad37bdc
Corrina Cornish <corrina.cornish@gmail.com>
2024-04-26 12:13:19 +0200

    easter-egg page

---
+++ pages/easter-egg.js
@@ -0,0 +1,11 @@
+
+
+export default function EasterEgg() {
+    return (
+        <div className="easterEggDiv">
+            <h2>Thanks to the Copilot</h2>
+            <img className="bells" src="/resources/bells.png" alt="image of a coding cat"></img>
+        </div>
+    )
+
+}
